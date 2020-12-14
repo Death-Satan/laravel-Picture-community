e966e3d533820799b2c38ef15c599fb5c540d491
2771717608@qq.com <2771717608@qq.com>
2020-12-14 15:31:58 +0800

    项目初始化

---
+++ resources/lang/zh_CN/failed-job.php
@@ -0,0 +1,16 @@
+<?php 
+return [
+    'labels' => [
+        'FailedJob' => '异步队列管理',
+        'failed-job' => '异步队列管理',
+    ],
+    'fields' => [
+        'connection' => '数据库连接',
+        'queue' => '队列名',
+        'payload' => '载荷',
+        'exception' => '错误异常',
+        'failed_at' => '创建时间',
+    ],
+    'options' => [
+    ],
+];
